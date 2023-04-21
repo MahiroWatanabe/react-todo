559f9ba5abdcbb23432e9fd492c2a9d8dd6be943
MahiroWatanabe <konchikuwa2@gmail.com>
2023-04-21 21:36:18 +0900

    InputForm.jsx done

--- src/components/InputForm.jsx
+++ src/components/InputForm.jsx
@@ -1,16 +1,31 @@
 import React from "react";
+import { useState } from "react";
 
-export const InputForm = () => {
+export const InputForm = ({ taskList, setTaskList }) => {
+  const [inputText, setInputText] = useState("");
+
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    setTaskList([
+      ...taskList,
+      {
+        id: taskList.length,
+        text: inputText,
+        completed: false,
+      },
+    ]);
+    setInputText("");
   };
 
-  const handleChange = (e) => {};
+  const handleChange = (e) => {
+    setInputText(e.target.value);
+  };
 
   return (
     <div className="inputForm">
-      <form onSubmit={handleSubmit} onChange={handleChange} action="">
-        <input type="text" />
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={handleChange} value={inputText} />
         <button>
           <i className="fas fa-plus-square"></i>
         </button>
