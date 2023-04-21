6f4f4affc7f1754cbc96a37dc79110247b29b8b2
MahiroWatanabe <konchikuwa2@gmail.com>
2023-04-21 21:36:42 +0900

    TodoList.jsx done

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,23 +1,46 @@
-import React from 'react'
+import React from "react";
 
-export const TodoList = () => {
+export const TodoList = ({ taskList, setTaskList }) => {
+  const handleDelete = (id) => {
+    setTaskList(taskList.filter((task) => task.id !== id));
+  };
+
+  const handleCheck = (id) => {
+    setTaskList(
+      taskList.map((task) => {
+        if (id === task.id) {
+          return {
+            ...task,
+            completed: !task.completed,
+          };
+        }
+        return task;
+      })
+    );
+  };
+
   return (
-      <div className='todoList'>
-          <div className='todos'>
-              <div className='todo'>
-                  <div className='todoText'>
-                      <span>プログラミング</span>
-                  </div>
-                  <div className="icons">
-                      <button>
-                          <i className="fas fa-check"></i>
-                      </button>
-                      <button>
-                          <i className="fas fa-trash"></i>
-                      </button>
-                  </div>
-              </div>
+    <div className="todoList">
+      <div className="todos">
+        {taskList.map((task, index) => (
+          <div
+            className={`todo ${task.completed ? "completed" : ""}`}
+            key={index}
+          >
+            <div className="todoText">
+              <span>{task.text}</span>
+            </div>
+            <div className="icons">
+              <button onClick={() => handleCheck(task.id)}>
+                <i className="fas fa-check"></i>
+              </button>
+              <button onClick={() => handleDelete(task.id)}>
+                <i className="fas fa-trash"></i>
+              </button>
+            </div>
           </div>
+        ))}
       </div>
-  )
-}
+    </div>
+  );
+};
